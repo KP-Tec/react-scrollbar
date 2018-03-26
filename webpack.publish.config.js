6ac7c0c7ecb135f9330b088b2e5ca55982bf9d56
mario <mfahlandt@pixel-haufen.de>
2018-03-26 16:46:57 +0200

    change copy task

--- webpack.publish.config.js
+++ webpack.publish.config.js
@@ -1,54 +1,56 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports =
-{
+  {
 
-  entry: './src',
+    entry: './src',
 
-  output: {
-    path: '/dist',
-    filename: 'react-scrollbar.js',
-    library: 'react-scrollbar',
-    libraryTarget: 'umd',
-  },
-
-  externals: {
-    react: 'react',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.s[a|c]ss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-
-  resolve: {
-    extensions: ['.js', '.jsx', '.scss', '.css'],
-    alias: {
-      'react-scrollbar-js': path.resolve(__dirname, 'src/'),
+    output: {
+      path: '/dist',
+      filename: 'react-scrollbar.js',
+      library: 'react-scrollbar',
+      libraryTarget: 'umd',
     },
-  },
 
-  devtool: 'source-map',
+    externals: {
+      react: 'react',
+    },
 
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.s[a|c]ss$/,
+          loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        }
+      ],
+    },
+    plugins: [new ExtractTextPlugin("doc/examples")],
 
-  devServer: {
-    contentBase: path.resolve(__dirname, 'example/public'),
-    publicPath: '/build/',
-    clientLogLevel: 'none',
+    resolve: {
+      extensions: ['.js', '.jsx', '.scss', '.css'],
+      alias: {
+        'react-scrollbar-js': path.resolve(__dirname, 'src/'),
+      },
+    },
+
+    devtool: 'source-map',
+
+    devServer: {
+      contentBase: path.resolve(__dirname, 'example/public'),
+      publicPath: '/build/',
+      clientLogLevel: 'none',
 //    inline: true,
 //    hot: true
-  },
+    },
 
-};
+  };
